312aa55873ebeff6d100f0a81ea61936f25bebe4
JohnBr2017 <goomba2001.jb@gmail.com>
2018-03-21 21:09:47 -0600

    deploying app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const expressJwt = require("express-jwt")
 
 const config = require("./config")
 
+const path = require("path")
 
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017",()=>{
@@ -20,6 +21,11 @@ app.use("/auth", require("./routes/auth"));
 app.use("/api", expressJwt({secret: process.env.SECRET}));
 app.use("/spells", require("./routes/spells"))
 app.use("/api/player", require("./routes/player"))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {  
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 //app.listener
 app.listen(config.port, ()=>{
     console.log("Listening on port " + config.port)
